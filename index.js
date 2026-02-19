9cf2e15939aedd6f63124b63a4ba2c39fff0e8f2
Yash-Rawat-IMA <yash97181004@gmail.com>
2026-02-19 23:14:40 +0530

    Added nodemon, .dotenv file for understanding

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./.env",
+});
+
+const myUserName = process.env.name;
+
+console.log("Value: ", myUserName);
+
+const db = process.env.database;
+
+console.log("Database: ", db);
+
 console.log("Start of Backend Project");
+
+console.log("Going Great with Backend Project");
